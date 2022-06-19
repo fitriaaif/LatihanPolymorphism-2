c4e4b609e0173a9b21c9fc608d3f573a3b8bc2e6
Fitria Amanda <101631102+fitriaaif@users.noreply.github.com>
2022-06-19 18:10:30 +0700

    Add files via upload

---
+++ ConvertToCsv.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanPolymorphism2
+{
+    public class ConvertToCsv : IConvertObject
+    {
+        public void Convert(Mahasiswa mhs)
+        {
+            // konversi objek mahasiswa ke CSV
+            string csv = string.Format("{0},{1},{2}", mhs.npm, mhs.nama, mhs.email);
+
+            Console.WriteLine("\nHasil konversi ke CSV: ");
+            Console.WriteLine(csv);
+        }
+    }
+}
---
+++ ConvertToJson.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace LatihanPolymorphism2
+{
+    public class ConvertToJson : IConvertObject
+    {
+        public void Convert(Mahasiswa mhs)
+        {
+            // konversi objek mahasiswa ke Json
+            string json = JsonConvert.SerializeObject(mhs);
+
+            Console.WriteLine("\nHasil konversi ke Json: ");
+            Console.WriteLine(json);
+        }
+    }
+}
